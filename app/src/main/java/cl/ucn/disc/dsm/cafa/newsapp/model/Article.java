854039893d925cb01442329d860c520252e2657c
Christian Farias <kurisu615@gmail.com>
2018-12-24 01:12:00 -0300

    Added: Modelo Article + Modelo Source.

--- app/src/main/java/cl/ucn/disc/dsm/cafa/newsapp/model/Article.java
+++ app/src/main/java/cl/ucn/disc/dsm/cafa/newsapp/model/Article.java
@@ -1,4 +1,76 @@
 package cl.ucn.disc.dsm.cafa.newsapp.model;
 
+import android.arch.persistence.room.Embedded;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity(tableName = "article")
 public class Article {
+
+    /**
+     * PK
+     */
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    /**
+     * La fuente del articulo.
+     */
+    @Embedded
+    @Getter
+    @Setter
+    private Source source;
+
+    /**
+     * El autor del articulo.
+     */
+    @Getter
+    @Setter
+    private String author;
+
+    /**
+     * El titulo.
+     */
+    @Getter
+    @Setter
+    private String title;
+
+    /**
+     * Una breve descripcion del articulo.
+     */
+    @Getter
+    @Setter
+    private String description;
+
+    /**
+     * La direccion al articulo.
+     */
+    @Getter
+    @Setter
+    private String url;
+
+    /**
+     * La direccion a la imagen del articulo.
+     */
+    @Getter
+    @Setter
+    private String urlToImage;
+
+    /**
+     * La fecha de publicacion.
+     */
+    @Getter
+    @Setter
+    private String publishedAt;
+
+    /**
+     * El contenido del articulo.
+     */
+    @Getter
+    @Setter
+    private String content;
+
 }
---
+++ app/src/main/java/cl/ucn/disc/dsm/cafa/newsapp/model/Source.java
@@ -0,0 +1,18 @@
+package cl.ucn.disc.dsm.cafa.newsapp.model;
+
+import lombok.Getter;
+
+public class Source {
+
+    /**
+     * El id de la fuente.
+     */
+    @Getter
+    private String id;
+
+    /**
+     * El nombre de la fuente.
+     */
+    @Getter
+    private String name;
+}
